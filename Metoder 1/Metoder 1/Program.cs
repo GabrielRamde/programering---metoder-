da436b19be51780cc78d18a1456bcf26351beea5
gabriel ramde <gara02001@utb.vaxjo.se>
2019-09-27 08:56:23 +0200

    Gjorde klart metoder 1

---
+++ Metoder 1/Metoder 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+        }
+
+        static double CirkelOmkrets(double radie)
+        {
+            return (radie * 2 * 3.14);
+
+        }
+        static double CirkelArea(double radie)
+        {
+            return (radie * radie * 3.14);
+        }
+
+
+    }
+}
